bfa8129eda0bd79fe77d9e1edef4e8a780d63d7c
landofcoder <landofcoder@gmail.com>
2022-05-03 20:55:53 +0700

    updated files

--- Filter/AbstractTranslitUrl.php
+++ Filter/AbstractTranslitUrl.php
@@ -1,18 +1,18 @@
 <?php
 /**
  * Landofcoder
- * 
+ *
  * NOTICE OF LICENSE
- * 
+ *
  * This source file is subject to the Landofcoder.com license that is
  * available through the world-wide-web at this URL:
  * https://landofcoder.com/license
- * 
+ *
  * DISCLAIMER
- * 
+ *
  * Do not edit or add to this file if you wish to upgrade this extension to newer
  * version in the future.
- * 
+ *
  * @category   Landofcoder
  * @package    Lof_UnicodeUrl
  * @copyright  Copyright (c) 2021 Landofcoder (https://landofcoder.com/)
@@ -24,8 +24,17 @@ use LanguageDetection\Language;
 
 class AbstractTranslitUrl implements \Zend_Filter_Interface
 {
+    /**
+     * filter string
+     *
+     * @param string|null $value
+     * @return string|null
+     */
     public function filter($value)
     {
+        if (empty($value)) {
+            return $value;
+        }
         $ld = new Language;
         $languages = $ld->detect($value)->close();
         if ($languages && count($languages) >= 3) {
@@ -41,7 +50,13 @@ class AbstractTranslitUrl implements \Zend_Filter_Interface
         }
         return urldecode($this->sanitize($value));
     }
-    
+
+    /**
+     * filter russian string
+     *
+     * @param string $value
+     * @return string
+     */
     public function filterRussian($value)
     {
         return $this->transliterate($value);
@@ -162,6 +177,12 @@ class AbstractTranslitUrl implements \Zend_Filter_Interface
 
     }
 
+    /**
+     * mb string endcoding
+     * @param mixed $reset
+     * @return mixed
+     *
+     */
     protected function mbstringBinarySafeEncoding($reset)
     {
         static $encodings = array();
@@ -185,11 +206,16 @@ class AbstractTranslitUrl implements \Zend_Filter_Interface
         }
     }
 
-    protected function transliterate($string) {
+    /**
+     * transliterate
+     *
+     * @param string $string
+     * @return string
+     */
+    protected function transliterate($string)
+    {
         $roman = array("Sch","sch",'Yo','Zh','Kh','Ts','Ch','Sh','Yu','ya','yo','zh','kh','ts','ch','sh','yu','ya','A','B','V','G','D','E','Z','I','Y','K','L','M','N','O','P','R','S','T','U','F','','Y','','E','a','b','v','g','d','e','z','i','y','k','l','m','n','o','p','r','s','t','u','f','','y','','e');
         $cyrillic = array("Щ","щ",'Ё','Ж','Х','Ц','Ч','Ш','Ю','я','ё','ж','х','ц','ч','ш','ю','я','А','Б','В','Г','Д','Е','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Ь','Ы','Ъ','Э','а','б','в','г','д','е','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','ь','ы','ъ','э');
         return str_replace($cyrillic, $roman, $string);
     }
-
-
 }
